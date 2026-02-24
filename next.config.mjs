d63f08d99cc112319ed1545d3c2e223b9af7caf4
jamestfilmer-ops <jamestfilmer@gmail.com>
2026-02-23 19:57:55 -0600

    Remove duplicate app folder

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: [
+      'is1-ssl.mzstatic.com', 'is2-ssl.mzstatic.com',
+      'is3-ssl.mzstatic.com', 'is4-ssl.mzstatic.com', 'is5-ssl.mzstatic.com'
+    ],
+  },
+}
+
+export default nextConfig
